0f7e3b20e3744756c32945121639e709738f5a85
Soebb <87156166+Soebb@users.noreply.github.com>
2021-07-10 15:22:57 +0430

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
-
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 API_ID = int(os.environ.get("API_ID", 12345))
 API_HASH = os.environ.get("API_HASH", "")
+
+db_url = "sqlite://userlanguages.db"
